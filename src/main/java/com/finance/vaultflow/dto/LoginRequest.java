7199aea1beb57392ea0f4dffe6088a9be482d483
Surya Gupta <181776742+Suryaguptaa@users.noreply.github.com>
2026-04-04 19:49:13 +0530

    feat: login request dto, nothing fancy

---
+++ src/main/java/com/finance/vaultflow/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.finance.vaultflow.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+}
