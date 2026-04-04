7b72a9e85c7e45ecda15e4833830108d1edc495f
Surya Gupta <181776742+Suryaguptaa@users.noreply.github.com>
2026-04-04 13:24:35 +0530

    Initial project setup

---
+++ src/test/java/com/finance/vaultflow/VaultflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.vaultflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
