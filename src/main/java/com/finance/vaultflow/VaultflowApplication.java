3e03d56daf072f0f29a99852b1de90a8491f9135
Surya Gupta <181776742+Suryaguptaa@users.noreply.github.com>
2026-04-04 21:20:05 +0530

    chore: main class, nothing to see here

--- src/main/java/com/finance/vaultflow/VaultflowApplication.java
+++ src/main/java/com/finance/vaultflow/VaultflowApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VaultflowApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VaultflowApplication.class, args);
 	}
-
 }
